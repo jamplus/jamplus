6ca5faf388ce063049a192ebbed13ebab32ad802
Joshua Jensen <jjensen@workspacewhiz.com>
2008-10-03 23:35:49 -0600

    * helloworld test that is slightly more complicated than the helloworld sample app.

---
+++ tests/helloworld/file.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void Print()
+{
+	printf("Something.\n");
+}
---
+++ tests/helloworld/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern void Print();
+
+int main()
+{
+	printf("Hello, world!\n");
+	Print();
+	return 0;
+}
