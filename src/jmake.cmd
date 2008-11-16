740b97b7d0b234a6e803bc120ecb2ee85bc8c88a
Alen Ladavac <alen.ladavac@gmail.com>
2008-11-16 18:33:57 +0100

    jmake.cmd now detects VS2005 or VS2008, so starting vsvars32.bat is not needed.

--- src/jmake.cmd
+++ src/jmake.cmd
@@ -1,4 +1,16 @@
+@echo off
+if NOT "%VS90COMNTOOLS%" == "" set COMNTOOLS=%VS90COMNTOOLS%
+if NOT "%VS80COMNTOOLS%" == "" set COMNTOOLS=%VS80COMNTOOLS%
+if "%COMNTOOLS%" == "" ( 
+ echo Cannot find Visual Studio 2005/2008. Aborting.
+ goto :end 
+)
+
+call "%COMNTOOLS%vsvars32.bat"
+
 nmake /f Makefile.Windows
 bin.ntx86\jam.exe -sCONFIG=release -sBUILD_J=yes
 @echo jam.exe is at bin.ntx86\jam.exe
 @echo j.exe is at jbin.ntx86\j.exe
+
+:end
