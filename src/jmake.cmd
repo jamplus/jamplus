f29e13e698ef0382bd5bd7d86100cd5c7951b826
Alen Ladavac <alen.ladavac@gmail.com>
2008-11-20 21:16:31 +0100

    jmake.cmd now checks for different versions of MSVC using both registry values and envvars.

--- src/jmake.cmd
+++ src/jmake.cmd
@@ -1,9 +1,66 @@
 @echo off
-if NOT "%VS90COMNTOOLS%" == "" set COMNTOOLS=%VS90COMNTOOLS%
-if NOT "%VS80COMNTOOLS%" == "" set COMNTOOLS=%VS80COMNTOOLS%
+
+REM Test for Visual Studio 2008
+
+(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\Microsoft\VisualStudio\9.0 /v InstallDir') do set VSDir=%%k) 2>nul
+
+if NOT "%VSDir%" == "" ( 
+  set COMNTOOLS=%VSDir%\..\..\Common7\
+  goto :foundenv
+)
+
+if NOT "%VS90COMNTOOLS%" == "" ( 
+  set COMNTOOLS=%VS90COMNTOOLS%
+  goto :foundenv
+)
+
+
+REM Test for Visual Studio 2005
+
+(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\Microsoft\VisualStudio\8.0 /v InstallDir') do set VSDir=%%k) 2>nul
+
+if NOT "%VSDir%" == "" ( 
+  set COMNTOOLS=%VSDir%\..\..\Common7\
+  goto :foundenv
+)
+
+if NOT "%VS80COMNTOOLS%" == "" ( 
+  set COMNTOOLS=%VS80COMNTOOLS%
+  goto :foundenv
+)
+
+REM Test for Visual Studio 2003
+
+(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\Microsoft\VisualStudio\7.1 /v InstallDir') do set VSDir=%%k) 2>nul
+
+if NOT "%VSDir%" == "" ( 
+  set COMNTOOLS=%VSDir%\..\..\Common7\
+  goto :foundenv
+)
+
+if NOT "%VS71COMNTOOLS%" == "" ( 
+  set COMNTOOLS=%VS71COMNTOOLS%
+  goto :foundenv
+)
+
+REM Test for Visual Studio 2002
+
+(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\Microsoft\VisualStudio\7.0 /v InstallDir') do set VSDir=%%k) 2>nul
+
+if NOT "%VSDir%" == "" ( 
+  set COMNTOOLS=%VSDir%\..\..\Common7\
+  goto :foundenv
+)
+
+if NOT "%VS70COMNTOOLS%" == "" ( 
+  set COMNTOOLS=%VS70COMNTOOLS%
+  goto :foundenv
+)
+
+:foundenv
 if "%COMNTOOLS%" == "" ( 
- echo Cannot find Visual Studio 2005/2008. Aborting.
- goto :end 
+ echo Cannot find Visual Studio. Aborting.
+ goto :end
 )
 
 if "%VCINSTALLDIR%" == "" call "%COMNTOOLS%vsvars32.bat"
