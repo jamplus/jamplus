c66200deafd16dbe583d0c29ebcda1aef4a83ecf
Alen Ladavac <alen.ladavac@gmail.com>
2008-11-24 20:15:36 +0100

    Removed calls to build j.exe from jmake.cmd.

--- src/jmake.cmd
+++ src/jmake.cmd
@@ -66,8 +66,6 @@ if "%COMNTOOLS%" == "" (
 if "%VCINSTALLDIR%" == "" call "%COMNTOOLS%vsvars32.bat"
 
 nmake /f Makefile.Windows
-bin.ntx86\jam.exe -sCONFIG=release -sBUILD_J=yes
 @echo jam.exe is at bin.ntx86\jam.exe
-@echo j.exe is at jbin.ntx86\j.exe
 
 :end
