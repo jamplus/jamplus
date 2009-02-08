60620f1eb1f7e2613cde5635a50ea87e8f0a9241
Joshua Jensen <jjensen@workspacewhiz.com>
2009-02-08 01:06:01 -0700

    * Some tests/sharedlib/ fixes.

--- tests/sharedlib/slib-a/slib-a.c
+++ tests/sharedlib/slib-a/slib-a.c
@@ -8,3 +8,13 @@ void ExportA()
 {
 	printf("ExportA: 2 + 5 = %d\n", Add(2, 5));
 }
+
+
+
+#if _MSC_VER
+__declspec(dllexport)
+#endif
+void ExportA2()
+{
+	printf("ExportA2: 3 + 9 = %d\n", Add(3, 9));
+}
