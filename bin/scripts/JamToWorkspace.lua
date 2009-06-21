3d6fd35f5a7e7cca9ba8cc800312523613759d47
Joshua Jensen <jjensen@workspacewhiz.com>
2009-06-20 23:41:30 -0600

    * JamToWorkspace: Fix the quoting in the .vcproj generation.

--- bin/scripts/JamToWorkspace.lua
+++ bin/scripts/JamToWorkspace.lua
@@ -351,9 +351,9 @@ function VisualStudioProjectMetaTable:Write(outputPath, commandLines)
 			configInfo.CleanCommandLine = commandLines[3] or ''
 		end
 		
-		configInfo.BuildCommandLine = configInfo.BuildCommandLine:gsub('"', '\\&quot;')
-		configInfo.RebuildCommandLine = configInfo.RebuildCommandLine:gsub('"', '\\&quot;')
-		configInfo.CleanCommandLine = configInfo.CleanCommandLine:gsub('"', '\\&quot;')
+		configInfo.BuildCommandLine = configInfo.BuildCommandLine:gsub('"', '&quot;')
+		configInfo.RebuildCommandLine = configInfo.RebuildCommandLine:gsub('"', '&quot;')
+		configInfo.CleanCommandLine = configInfo.CleanCommandLine:gsub('"', '&quot;')
 
 		if self.Options.vs2003 then
 			table.insert(self.Contents, expand([==[
