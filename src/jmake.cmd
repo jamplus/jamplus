17baf62982147a11f33fd42b62f8f1141a35dffb
Joshua Jensen <jjensen@workspacewhiz.com>
2009-07-21 20:27:32 -0600

    * Jambase: Visual Studio 2010 support provided by Philipp Teschner. * Bootstrapping: Visual Studio 2010 support.

--- src/jmake.cmd
+++ src/jmake.cmd
@@ -1,70 +1,159 @@
 @echo off
 setlocal
 
-REM Test for Visual Studio 2008
+REM Determine command line variables (/V or /v for verbose output)
 
-(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\Microsoft\VisualStudio\9.0 /v InstallDir') do set VSDir=%%k) 2>nul
+set VERBOSE=0
+
+if "%1" == "/V" (
+  set VERBOSE=1
+)
+
+if "%1" == "/v" (
+  set VERBOSE=1
+)
+
+REM Setup 64-bit aware registry location
+
+IF NOT "%ProgramFiles(x86)%"=="" SET WOW6432NODE=WOW6432NODE\
+
+REM Test for Visual Studio 2010
+
+if %VERBOSE% == 1 echo.Checking Visual Studio 2010
+if %VERBOSE% == 1 echo.  ...looking in registry
+
+(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\%WOW6432NODE%Microsoft\VisualStudio\10.0 /v InstallDir') do set VSDir=%%k) 2>nul
+
+if %VERBOSE% == 1 echo.  ...done
 
 if NOT "%VSDir%" == "" (
-  set COMNTOOLS=%VSDir%\..\..\Common7\Tools\
+  set VS=2010
+  set COMNTOOLS="%VSDir%..\..\Common7\Tools\"
   goto :foundenv
+)
+
+if %VERBOSE% == 1 echo.  ...looking in environment
+
+if NOT "%VS100COMNTOOLS%" == "" (
+  set VS=2009
+  set COMNTOOLS="%VS100COMNTOOLS%"
+  goto :foundenv
+)
+
+if %VERBOSE% == 1 echo   ...done
+
+REM Test for Visual Studio 2008
+
+if %VERBOSE% == 1 echo.Checking Visual Studio 2008
+if %VERBOSE% == 1 echo.  ...looking in registry
+
+(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\%WOW6432NODE%Microsoft\VisualStudio\9.0 /v InstallDir') do set VSDir=%%k) 2>nul
+
+if %VERBOSE% == 1 echo.  ...done
+
+if NOT "%VSDir%" == "" (
+  set VS=2008
+  set COMNTOOLS="%VSDir%..\..\Common7\Tools\"
+  goto :foundenv
+)
+
+if %VERBOSE% == 1 echo.  ...looking in environment
 )
 
 if NOT "%VS90COMNTOOLS%" == "" (
-  set COMNTOOLS=%VS90COMNTOOLS%
+  set VS=2008
+  set COMNTOOLS="%VS90COMNTOOLS%"
   goto :foundenv
 )
 
+if %VERBOSE% == 1 echo   ...done
 
 REM Test for Visual Studio 2005
 
-(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\Microsoft\VisualStudio\8.0 /v InstallDir') do set VSDir=%%k) 2>nul
+if %VERBOSE% == 1 echo.Checking Visual Studio 2005
+if %VERBOSE% == 1 echo.  ...looking in registry
+
+(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\%WOW6432NODE%Microsoft\VisualStudio\8.0 /v InstallDir') do set VSDir=%%k) 2>nul
+
+if %VERBOSE% == 1 echo.  ...done
 
 if NOT "%VSDir%" == "" (
-  set COMNTOOLS=%VSDir%\..\..\Common7\Tools\
+  set VS=2005
+  set COMNTOOLS="%VSDir%..\..\Common7\Tools\"
   goto :foundenv
 )
 
+if %VERBOSE% == 1 echo.  ...looking in environment
+
 if NOT "%VS80COMNTOOLS%" == "" (
-  set COMNTOOLS=%VS80COMNTOOLS%
+  set VS=2005
+  set COMNTOOLS="%VS80COMNTOOLS%"
   goto :foundenv
 )
+
+if %VERBOSE% == 1 echo.  ...done
 
 REM Test for Visual Studio 2003
 
-(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\Microsoft\VisualStudio\7.1 /v InstallDir') do set VSDir=%%k) 2>nul
+if %VERBOSE% == 1 echo.Checking Visual Studio 2003
+if %VERBOSE% == 1 echo.  ...looking in registry
+
+(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\%WOW6432NODE%Microsoft\VisualStudio\7.1 /v InstallDir') do set VSDir=%%k) 2>nul
+
+if %VERBOSE% == 1 echo.  ...done
 
 if NOT "%VSDir%" == "" (
-  set COMNTOOLS=%VSDir%\..\..\Common7\Tools\
+  set VS=2003
+  set COMNTOOLS="%VSDir%..\..\Common7\Tools\"
   goto :foundenv
 )
 
+if %VERBOSE% == 1 echo.  ...looking in environment
+
 if NOT "%VS71COMNTOOLS%" == "" (
-  set COMNTOOLS=%VS71COMNTOOLS%
+  set VS=2003
+  set COMNTOOLS="%VS71COMNTOOLS%"
   goto :foundenv
 )
+
+if %VERBOSE% == 1 echo   ...done
 
 REM Test for Visual Studio 2002
 
-(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\Microsoft\VisualStudio\7.0 /v InstallDir') do set VSDir=%%k) 2>nul
+if %VERBOSE% == 1 echo.Checking Visual Studio 2002
+if %VERBOSE% == 1 echo.  ...looking in registry
+
+(for /f "tokens=1,2*" %%i in ('reg query HKLM\software\%WOW6432NODE%Microsoft\VisualStudio\7.0 /v InstallDir') do set VSDir=%%k) 2>nul
+
+if %VERBOSE% == 1 echo.  ...done
 
 if NOT "%VSDir%" == "" (
-  set COMNTOOLS=%VSDir%\..\..\Common7\Tools\
+  set VS=2002
+  set COMNTOOLS="%VSDir%..\..\Common7\Tools\"
   goto :foundenv
 )
+
+if %VERBOSE% == 1 echo.  ...looking in environment
 
 if NOT "%VS70COMNTOOLS%" == "" (
-  set COMNTOOLS=%VS70COMNTOOLS%
+  set VS=2002
+  set COMNTOOLS="%VS70COMNTOOLS%"
   goto :foundenv
 )
 
+if %VERBOSE% == 1 echo.  ...done
+
 :foundenv
-if "%COMNTOOLS%" == "" (
+
+if %COMNTOOLS% == "" (
  echo Cannot find Visual Studio. Aborting.
  goto :end
 )
 
-if "%VCINSTALLDIR%" == "" call "%COMNTOOLS%vsvars32.bat"
+if %VERBOSE% == 1 echo.Found Visual Studio %VS%
+if %VERBOSE% == 1 echo.
+
+if "%VCINSTALLDIR%" == "" call %COMNTOOLS%vsvars32.bat
 
 nmake /f Makefile.Windows
 @echo jam.exe is at ..\bin\jam.exe
