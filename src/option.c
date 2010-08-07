ebee96eb3b878943f458fd0e9dd68dd46c62de64
Joshua Jensen <jjensen@workspacewhiz.com>
2010-08-07 11:39:23 -0600

    * Reformat whitespace in option.c.

--- src/option.c
+++ src/option.c
@@ -26,63 +26,63 @@ getoptions(
     const char *opts,
     option *optv )
 {
-    int i;
-    int optc = N_OPTS;
+	int i;
+	int optc = N_OPTS;
 
-    memset( (char *)optv, '\0', sizeof( *optv ) * N_OPTS );
+	memset( (char *)optv, '\0', sizeof( *optv ) * N_OPTS );
 
-    for( i = 0; i < argc; i++ )
-    {
-	char *arg;
-
-	if( argv[i][0] != '-' || !isalpha( argv[i][1] ) )
-	    break;
-
-	if( !optc-- )
+	for( i = 0; i < argc; i++ )
 	{
-	    printf( "too many options (%d max)\n", N_OPTS );
-	    return -1;
+		char *arg;
+
+		if( argv[i][0] != '-' || !isalpha( argv[i][1] ) )
+			break;
+
+		if( !optc-- )
+		{
+			printf( "too many options (%d max)\n", N_OPTS );
+			return -1;
+		}
+
+		for( arg = &argv[i][1]; *arg; arg++ )
+		{
+			const char *f;
+
+			for( f = opts; *f; f++ )
+				if( *f == *arg )
+					break;
+
+			if( !*f )
+			{
+				printf( "Invalid option: -%c\n", *arg );
+				return -1;
+			}
+
+			optv->flag = *f;
+
+			if( f[1] != ':' )
+			{
+				optv++->val = "true";
+			}
+			else if( arg[1] )
+			{
+				optv++->val = &arg[1];
+				break;
+			}
+			else if( ++i < argc )
+			{
+				optv++->val = argv[i];
+				break;
+			}
+			else
+			{
+				printf( "option: -%c needs argument\n", *f );
+				return -1;
+			}
+		}
 	}
 
-	for( arg = &argv[i][1]; *arg; arg++ )
-	{
-	    const char *f;
-
-	    for( f = opts; *f; f++ )
-		if( *f == *arg )
-		    break;
-
-	    if( !*f )
-	    {
-		printf( "Invalid option: -%c\n", *arg );
-		return -1;
-	    }
-
-	    optv->flag = *f;
-
-	    if( f[1] != ':' )
-	    {
-		optv++->val = "true";
-	    }
-	    else if( arg[1] )
-	    {
-		optv++->val = &arg[1];
-		break;
-	    }
-	    else if( ++i < argc )
-	    {
-		optv++->val = argv[i];
-		break;
-	    }
-	    else
-	    {
-		printf( "option: -%c needs argument\n", *f );
-		return -1;
-	    }
-	}
-    }
-
-    return i;
+	return i;
 }
 
 /*
@@ -98,8 +98,8 @@ getoptval(
 	int i;
 
 	for( i = 0; i < N_OPTS; i++, optv++ )
-	    if( optv->flag == opt && !subopt-- )
-		return optv->val;
+		if( optv->flag == opt && !subopt-- )
+			return optv->val;
 
 	return 0;
 }
