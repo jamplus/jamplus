611f62896696f3bff3d50f5d6f51ac073c85080e
Joshua Jensen <jjensen@workspacewhiz.com>
2011-01-31 20:11:44 -0700

    * Tests: Add generatedc test.

---
+++ tests/generatedc/test.lua
@@ -0,0 +1,123 @@
+function Test()
+	-- Test for a clean directory.
+	local originalFiles =
+	{
+		'Jamfile.jam',
+		'file.txt',
+		'main.c',
+	}
+
+	local originalDirs =
+	{
+	}
+
+	RunJam{ 'clean' }
+	TestDirectories(originalDirs)
+	TestFiles(originalFiles)
+
+	if Platform == 'win32' then
+		-- First build
+		local pattern = [[
+*** found 15 target(s)...
+*** updating 4 target(s)...
+@ GenerateCFile <helloworld>file.c
+!NEXT!@ C.CC <helloworld>main.obj
+main.c
+file.c
+Generating Code...
+@ C.LinkWithManifest <helloworld>helloworld.release.exe
+*** updated 4 target(s)...
+]]
+
+		TestPattern(pattern, RunJam())
+
+		local pass1Files =
+		{
+			'Jamfile.jam',
+			'file.c',
+			'file.obj',
+			'file.txt',
+			'helloworld.release.exe',
+			'helloworld.release.exe.intermediate.manifest',
+			'helloworld.release.pdb',
+			'main.c',
+			'main.obj',
+			'vc.pdb',
+		}
+
+		TestFiles(pass1Files)
+		TestDirectories(originalDirs)
+
+		local pattern2 = [[
+*** found 15 target(s)...
+]]
+		TestPattern(pattern2, RunJam())
+
+		os.sleep(1.0)
+		os.touch('file.c')
+
+		local pattern3 = [[
+*** found 15 target(s)...
+*** updating 2 target(s)...
+@ C.CC <helloworld>file.obj
+file.c
+@ C.LinkWithManifest <helloworld>helloworld.release.exe
+*** updated 2 target(s)...
+]]
+		TestPattern(pattern3, RunJam())
+		TestFiles(pass1Files)
+		TestDirectories(originalDirs)
+
+	else
+        error("Not created on non-Windows platforms yet")
+		-- First build
+		local pattern = [[
+*** found 9 target(s)...
+*** updating 3 target(s)...
+@ C.CC <helloworld>main.o
+@ C.CC <helloworld>file.o
+@ C.Link <helloworld>helloworld.release
+*** updated 3 target(s)...
+]]
+
+		TestPattern(pattern, RunJam())
+
+		local pass1Files =
+		{
+			'Jamfile.jam',
+			'file.c',
+			'file.o',
+			'helloworld.release',
+			'main.c',
+			'main.o',
+		}
+
+		TestFiles(pass1Files)
+		TestDirectories(originalDirs)
+
+		local pattern2 = [[
+*** found 9 target(s)...
+]]
+		TestPattern(pattern2, RunJam())
+
+		os.sleep(1.0)
+		os.touch('file.c')
+
+		local pattern3 = [[
+*** found 9 target(s)...
+*** updating 2 target(s)...
+@ C.CC <helloworld>file.o
+@ C.Link <helloworld>helloworld.release
+*** updated 2 target(s)...
+]]
+		TestPattern(pattern3, RunJam())
+		TestFiles(pass1Files)
+		TestDirectories(originalDirs)
+	end
+
+	RunJam{ 'clean' }
+
+	TestDirectories(originalDirs)
+	TestFiles(originalFiles)
+end
+
