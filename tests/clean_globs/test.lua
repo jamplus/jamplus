b42f6a490cc045e40a1224a86a3f4d33f77e42b7
Joshua Jensen <jjensen@workspacewhiz.com>
2014-02-16 00:14:50 -0700

    Tests: Add new test clean_globs

---
+++ tests/clean_globs/test.lua
@@ -0,0 +1,305 @@
+function Test()
+    RunJam{ 'clean' }
+
+    local cleanFiles =
+    {
+        'Jamfile.jam',
+    }
+    TestFiles(cleanFiles)
+
+    local run1pattern =
+    {
+        '*** found 8 target(s)...',
+        '*** updating 8 target(s)...',
+        '@ WriteFile out.txt',
+        '@ WriteFile out2.txt',
+        '@ WriteExtraFiles all',
+        '*** updated 8 target(s)...',
+    }
+
+    TestPattern(run1pattern, RunJam())
+
+    local allFiles = {
+        'Jamfile.jam',
+        'subdir1/junk.txt',
+        'subdir1/subdir2/out2.txt',
+        'subdira/extrafile.txt',
+        'subdira/junk.txt',
+        'subdira/subdirb/subdirc/out.txt',
+        'subdira/subdirb/subdirc/subdird/anotherjunk.txt',
+        'subdira/subdirb/subdirc/subdird/junk.txt',
+    }
+    TestFiles(allFiles)
+
+    local allDirectories = {
+        'subdir1/',
+        'subdir1/subdir2/',
+        'subdira/',
+        'subdira/subdirb/',
+        'subdira/subdirb/subdirc/',
+        'subdira/subdirb/subdirc/subdird/',
+    }
+    TestDirectories(allDirectories)
+
+    TestPattern({
+        '*** found 2 target(s)...',
+        '*** updating 1 target(s)...',
+        '@ Clean clean',
+        '@ CleanTree clean',
+        '*** updated 1 target(s)...',
+    }, RunJam{ 'clean' })
+
+    TestFiles(cleanFiles)
+
+    ---------------------------------------------
+    TestPattern({
+        '*** found 8 target(s)...',
+        '*** updating 8 target(s)...',
+        '@ WriteFile out.txt',
+        '@ WriteFile out2.txt',
+        '@ WriteExtraFiles all',
+        '*** updated 8 target(s)...',
+    }, RunJam())
+
+    TestFiles(allFiles)
+    TestDirectories(allDirectories)
+
+    ---------------------------------------------
+    TestPattern({
+        '*** found 8 target(s)...',
+        '*** updating 1 target(s)...',
+        '@ WriteExtraFiles all',
+        '*** updated 1 target(s)...',
+    }, RunJam())
+
+    TestFiles(allFiles)
+    TestDirectories(allDirectories)
+
+    ---------------------------------------------
+    TestPattern({
+        '*** found 8 target(s)...',
+        '*** updating 1 target(s)...',
+        '@ WriteExtraFiles all',
+        '*** updated 1 target(s)...',
+        'Removing subdira/extrafile.txt...',
+        'Removing subdira/subdirb/subdirc/subdird/anotherjunk.txt...',
+        'Removing subdira/subdirb/subdirc/subdird/junk.txt...',
+        'Removing subdir1/junk.txt...',
+    }, RunJam{'TEST_CLEAN_KEEP_GLOBS=1'})
+
+    TestFiles({
+        'Jamfile.jam',
+        'subdir1/subdir2/out2.txt',
+        'subdira/junk.txt',
+        'subdira/subdirb/subdirc/out.txt',
+    })
+
+    TestDirectories{
+        'subdir1/',
+        'subdir1/subdir2/',
+        'subdira/',
+        'subdira/subdirb/',
+        'subdira/subdirb/subdirc/',
+    }
+
+    ---------------------------------------------
+    RunJam()
+    TestFiles(allFiles)
+    TestDirectories(allDirectories)
+
+    ---------------------------------------------
+    TestPattern({
+        '*** found 8 target(s)...',
+        '*** updating 1 target(s)...',
+        '@ WriteExtraFiles all',
+        '*** updated 1 target(s)...',
+        'Removing subdira/subdirb/subdirc/subdird/anotherjunk.txt...',
+        'Removing subdira/subdirb/subdirc/subdird/junk.txt...',
+        'Removing subdir1/junk.txt...',
+    }, RunJam{'TEST_CLEAN_KEEP_GLOBS=2'})
+
+    TestFiles({
+        'Jamfile.jam',
+        'subdir1/subdir2/out2.txt',
+        'subdira/extrafile.txt',
+        'subdira/junk.txt',
+        'subdira/subdirb/subdirc/out.txt',
+    })
+
+    TestDirectories{
+        'subdir1/',
+        'subdir1/subdir2/',
+        'subdira/',
+        'subdira/subdirb/',
+        'subdira/subdirb/subdirc/',
+    }
+
+    ---------------------------------------------
+    RunJam()
+    TestFiles(allFiles)
+    TestDirectories(allDirectories)
+
+    -----------------------------------
+    TestPattern({
+        '*** found 8 target(s)...',
+        '*** updating 1 target(s)...',
+        '@ WriteExtraFiles all',
+        '*** updated 1 target(s)...',
+        'Removing subdir1/junk.txt...',
+    }, RunJam{'TEST_CLEAN_KEEP_GLOBS=3'})
+
+    TestFiles({
+        'Jamfile.jam',
+        'subdir1/subdir2/out2.txt',
+        'subdira/extrafile.txt',
+        'subdira/junk.txt',
+        'subdira/subdirb/subdirc/out.txt',
+        'subdira/subdirb/subdirc/subdird/anotherjunk.txt',
+        'subdira/subdirb/subdirc/subdird/junk.txt',
+    })
+
+    TestDirectories{
+        'subdir1/',
+        'subdir1/subdir2/',
+        'subdira/',
+        'subdira/subdirb/',
+        'subdira/subdirb/subdirc/',
+        'subdira/subdirb/subdirc/subdird/',
+    }
+
+    --------------------
+    RunJam()
+    TestFiles(allFiles)
+    TestDirectories(allDirectories)
+
+    -----------------------------------
+    TestPattern({
+        '*** found 8 target(s)...',
+        '*** updating 1 target(s)...',
+        '@ WriteExtraFiles all',
+        '*** updated 1 target(s)...',
+        'Removing subdira/extrafile.txt...',
+        'Removing subdira/junk.txt...',
+        'Removing subdira/subdirb/subdirc/subdird/anotherjunk.txt...',
+        'Removing subdira/subdirb/subdirc/subdird/junk.txt...',
+    }, RunJam{'TEST_CLEAN_KEEP_TARGETS=1'})
+
+    TestFiles({
+        'Jamfile.jam',
+        'subdir1/junk.txt',
+        'subdir1/subdir2/out2.txt',
+        'subdira/subdirb/subdirc/out.txt',
+    })
+
+    TestDirectories{
+        'subdir1/',
+        'subdir1/subdir2/',
+        'subdira/',
+        'subdira/subdirb/',
+        'subdira/subdirb/subdirc/',
+    }
+
+    --------------------
+    RunJam()
+    TestFiles(allFiles)
+    TestDirectories(allDirectories)
+
+    -----------------------------------
+    TestPattern({
+        '*** found 8 target(s)...',
+        '*** updating 1 target(s)...',
+        '@ WriteExtraFiles all',
+        '*** updated 1 target(s)...',
+        'Removing subdira/extrafile.txt...',
+        'Removing subdira/junk.txt...',
+        'Removing subdira/subdirb/subdirc/subdird/anotherjunk.txt...',
+        'Removing subdira/subdirb/subdirc/subdird/junk.txt...',
+        'Removing subdir1/junk.txt...',
+    }, RunJam{'TEST_CLEAN_WILDCARDS=1'})
+
+    TestFiles({
+        'Jamfile.jam',
+        'subdir1/subdir2/out2.txt',
+        'subdira/subdirb/subdirc/out.txt',
+    })
+
+    TestDirectories{
+        'subdir1/',
+        'subdir1/subdir2/',
+        'subdira/',
+        'subdira/subdirb/',
+        'subdira/subdirb/subdirc/',
+    }
+
+    --------------------
+    RunJam()
+    TestFiles(allFiles)
+    TestDirectories(allDirectories)
+
+    -----------------------------------
+    TestPattern({
+        '*** found 8 target(s)...',
+        '*** updating 1 target(s)...',
+        '@ WriteExtraFiles all',
+        '*** updated 1 target(s)...',
+        'Removing subdira/extrafile.txt...',
+        'Removing subdira/subdirb/subdirc/subdird/anotherjunk.txt...',
+        'Removing subdir1/junk.txt...',
+    }, RunJam{'TEST_CLEAN_WILDCARDS=2'})
+
+    TestFiles({
+        'Jamfile.jam',
+        'subdir1/subdir2/out2.txt',
+        'subdira/junk.txt',
+        'subdira/subdirb/subdirc/subdird/junk.txt',
+        'subdira/subdirb/subdirc/out.txt',
+    })
+
+    TestDirectories{
+        'subdir1/',
+        'subdir1/subdir2/',
+        'subdira/',
+        'subdira/subdirb/',
+        'subdira/subdirb/subdirc/',
+        'subdira/subdirb/subdirc/subdird/',
+    }
+
+    --------------------
+    RunJam()
+    TestFiles(allFiles)
+    TestDirectories(allDirectories)
+
+    -----------------------------------
+    TestPattern({
+        '*** found 8 target(s)...',
+        '*** updating 1 target(s)...',
+        '@ WriteExtraFiles all',
+        '*** updated 1 target(s)...',
+        'Removing subdira/extrafile.txt...',
+        'Removing subdir1/junk.txt...',
+    }, RunJam{'TEST_CLEAN_WILDCARDS=3'})
+
+    TestFiles({
+        'Jamfile.jam',
+        'subdir1/subdir2/out2.txt',
+        'subdira/junk.txt',
+        'subdira/subdirb/subdirc/subdird/anotherjunk.txt',
+        'subdira/subdirb/subdirc/subdird/junk.txt',
+        'subdira/subdirb/subdirc/out.txt',
+    })
+
+    TestDirectories{
+        'subdir1/',
+        'subdir1/subdir2/',
+        'subdira/',
+        'subdira/subdirb/',
+        'subdira/subdirb/subdirc/',
+        'subdira/subdirb/subdirc/subdird/',
+    }
+
+    --------------------
+    RunJam()
+    TestFiles(allFiles)
+    TestDirectories(allDirectories)
+end
+
