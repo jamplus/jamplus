5cde01f4fed18202eb58b99cf589aec43998f29b
Joshua Jensen <jjensen@workspacewhiz.com>
2015-02-04 18:05:17 -0700

    Bootstrapping: Add bootstrap-linux64.sh

---
+++ bootstrap-linux64.sh
@@ -0,0 +1,8 @@
+cd src
+make linux64
+cd luaplus
+../../bin/linux64/jam LUA_VERSION=lua52-luaplus C.TOOLCHAIN=linux64/release
+cd ..
+export LUA_BIN=luaplus/.build/bin.lua52-luaplus.gcc.linux64
+make linux64
+
