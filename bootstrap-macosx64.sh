f345242c34f04f93747ce4506655c3b28ad77d18
Joshua Jensen <jjensen@workspacewhiz.com>
2015-02-04 18:21:58 -0700

    Bootstrapping: Add bootstrap-macosx64.sh

---
+++ bootstrap-macosx64.sh
@@ -0,0 +1,9 @@
+cd src
+rm -rf bin.macosxx64 jam0
+make macosx64
+cd luaplus
+../../bin/macosx64/jam LUA_VERSION=lua52-luaplus C.TOOLCHAIN=macosx64/releaseltcg
+cd ..
+export LUA_BIN=luaplus/.build/bin.lua52-luaplus.clang.macosx64
+make macosx64
+
