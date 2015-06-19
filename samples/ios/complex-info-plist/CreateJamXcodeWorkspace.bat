972e8f122a46300b6b5e0c9736891f1e7a5b2d1b
Joshua Jensen <jjensen@workspacewhiz.com>
2015-06-18 22:14:48 -0600

    Samples: Add complex-info-plist

---
+++ samples/ios/complex-info-plist/CreateJamXcodeWorkspace.bat
@@ -0,0 +1,1 @@
+@jam --workspace --gui --gen=xcode --config=%~dp0CreateJamXcodeWorkspace.config Jamfile.jam %~dp0../../../build/ios/complex-info-plist
---
+++ samples/ios/complex-info-plist/CreateJamXcodeWorkspace.sh
@@ -0,0 +1,2 @@
+jam --workspace -gen=xcode -config=CreateJamXcodeWorkspace.config Jamfile.jam ../../../build/ios/complex-info-plist
+
