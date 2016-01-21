67aa4b3aa280d9b66e885f70515de3b8fa68b75d
Joshua Jensen <jjensen@workspacewhiz.com>
2016-01-21 16:41:57 -0700

    Tests: Add checksum/simple

---
+++ tests/checksum/simple/test.lua
@@ -0,0 +1,146 @@
+function TestChecksum()
+    local function WriteOriginalFiles()
+        ospath.write_file('template', [[
+#define VALUE 8
+]])
+    end
+
+    -- Test for a clean directory.
+    local originalFiles = {
+        'Jamfile.jam',
+        'main.c',
+        'template',
+    }
+
+    local originalDirs = {
+    }
+
+    -- Clean up everything.
+    WriteOriginalFiles()
+    ospath.remove('.jamcache')
+    RunJam{ 'clean' }
+    TestDirectories(originalDirs)
+    TestFiles(originalFiles)
+
+    ---------------------------------------------------------------------------
+    local files
+    local dirs = {
+        '$(TOOLCHAIN_PATH)/test/',
+    }
+
+    local function TestNoopPattern()
+        local noopPattern = [[
+*** found 10 target(s)...
+]]
+        TestPattern(noopPattern, RunJam{})
+        TestDirectories(dirs)
+        TestFiles(files)
+    end
+
+    local patternA
+    local patternB
+    local patternC
+    if Platform == 'win32' then
+        files = {
+            'generated.h',
+            'Jamfile.jam',
+            'main.c',
+            'template',
+            '$(TOOLCHAIN_PATH)/test/main.obj',
+            '$(TOOLCHAIN_PATH)/test/test.release.exe',
+            '?$(TOOLCHAIN_PATH)/test/test.release.exe.intermediate.manifest',
+            '$(TOOLCHAIN_PATH)/test/test.release.pdb',
+        }
+
+        patternA = [[
+*** found 21 target(s)...
+*** updating 4 target(s)...
+@ GenerateHeader <$(TOOLCHAIN_GRIST):test>generated.h
+@ $(C_CC) <$(TOOLCHAIN_GRIST):test>main.obj
+!NEXT!@ $(C_LINK) <$(TOOLCHAIN_GRIST):test>test.exe
+!NEXT!*** updated 4 target(s)...
+]]
+    else
+        files = {
+            'generated.h',
+            'Jamfile.jam',
+            'main.c',
+            'template',
+            '$(TOOLCHAIN_PATH)/test/main.o',
+            '$(TOOLCHAIN_PATH)/test/test.release',
+        }
+
+        patternA = [[
+*** found 10 target(s)...
+*** updating 4 target(s)...
+@ GenerateHeader <$(TOOLCHAIN_GRIST):test>generated.h
+@ $(C_CC) <$(TOOLCHAIN_GRIST):test>main.o 
+@ $(C_LINK) <$(TOOLCHAIN_GRIST):test>test
+*** updated 4 target(s)...
+]]
+    end
+
+    ---------------------------------------------------------------------------
+    do
+        TestPattern(patternA, RunJam{})
+        TestDirectories(dirs)
+        TestFiles(files)
+    end
+
+    ---------------------------------------------------------------------------
+    do
+        TestNoopPattern()
+    end
+
+    ---------------------------------------------------------------------------
+    do
+        TestNoopPattern()
+    end
+
+    ---------------------------------------------------------------------------
+    do
+        osprocess.sleep(1.0)
+        ospath.touch('template')
+        TestNoopPattern()
+    end
+
+    ---------------------------------------------------------------------------
+    do
+        local pattern
+        if Platform == 'win32' then
+            pattern = [[
+*** found 10 target(s)...
+*** updating 3 target(s)...
+@ GenerateHeader <$(TOOLCHAIN_GRIST):test>generated.h
+@ $(C_CC) <$(TOOLCHAIN_GRIST):test>main.obj
+!NEXT!@ $(C_LINK) <$(TOOLCHAIN_GRIST):test>test.exe
+!NEXT!*** updated 3 target(s)...
+]]
+        else
+            pattern = [[
+*** found 10 target(s)...
+*** updating 3 target(s)...
+@ GenerateHeader <$(TOOLCHAIN_GRIST):test>generated.h
+@ $(C_CC) <$(TOOLCHAIN_GRIST):test>main.o 
+*** updated 2 target(s)...
+]]
+        end
+
+        osprocess.sleep(1.0)
+        ospath.write_file('template', [[
+#define VALUE 10
+]])
+        TestPattern(pattern, RunJam{})
+    end
+
+    ---------------------------------------------------------------------------
+    do
+        TestNoopPattern()
+    end
+
+    ---------------------------------------------------------------------------
+    WriteOriginalFiles()
+    RunJam{ 'clean' }
+    TestFiles(originalFiles)
+    TestDirectories(originalDirs)
+end
