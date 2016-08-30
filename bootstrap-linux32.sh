52767a9a88d44f8f1362bf96492b87743e393b07
Joshua Jensen <jjensen@workspacewhiz.com>
2016-08-30 14:33:54 -0600

    Bootstrapping: Fix bootstrap-linux32.sh and bootstrap-linux64.sh

--- bootstrap-linux32.sh
+++ bootstrap-linux32.sh
@@ -1,9 +1,9 @@
 cd src
 make linux32
 cd luaplus
-../../bin/linux32/jam LUA_VERSION=lua52-luaplus
+../../bin/linux32/jam LUA_VERSION=lua53-luaplus C.TOOLCHAIN=linux32/release
 cd ..
-export LUA_BIN=luaplus/.build/bin.lua52-luaplus.gcc.linux32
+export LUA_BIN=luaplus/.build/linux32/bin
 make linux32
 export LUA_BIN=
 cd ..
--- bootstrap-linux64.sh
+++ bootstrap-linux64.sh
@@ -1,9 +1,9 @@
 cd src
 make linux64
 cd luaplus
-../../bin/linux64/jam LUA_VERSION=lua52-luaplus C.TOOLCHAIN=linux64/release
+../../bin/linux64/jam LUA_VERSION=lua53-luaplus C.TOOLCHAIN=linux64/release
 cd ..
-export LUA_BIN=luaplus/.build/bin.lua52-luaplus.gcc.linux64
+export LUA_BIN=luaplus/.build/linux64/bin
 make linux64
 export LUA_BIN=
 cd ..
