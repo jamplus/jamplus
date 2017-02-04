e6cd5508bbc514d42f793b97f7de31191ce22ec3
Joshua Jensen <jjensen@workspacewhiz.com>
2017-02-04 12:09:12 -0700

    Generate: Refuse to overwrite Jamfile.jam OR Jamfile.lua

--- bin/scripts/Generate.lua
+++ bin/scripts/Generate.lua
@@ -1,11 +1,19 @@
 local filefind = require 'filefind'
 local ospath = require 'ospath'
 
-local jamfileFilename = 'Jamfile.jam'
-if ospath.exists(jamfileFilename) then
+local jamfileJamFilename = 'Jamfile.jam'
+if ospath.exists(jamfileJamFilename) then
+    print('The file Jamfile.jam is already present and will not be overwritten.')
     return
 end
 
-local file = io.open(jamfileFilename, 'wb')
+local jamfileLuaFilename = 'Jamfile.lua'
+if ospath.exists(jamfileLuaFilename) then
+    print('The file Jamfile.lua is already present and will not be overwritten.')
+    return
+end
+
+print('Writing ' .. jamfileJamFilename .. '...')
+local file = io.open(jamfileJamFilename, 'wb')
 file:write('C.Application app : *.c *.cpp ;\n')
 file:close()
