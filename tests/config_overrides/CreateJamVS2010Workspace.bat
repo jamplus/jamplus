7a2e2189b73619af99e162a3437397553c036d02
Joshua Jensen <jjensen@workspacewhiz.com>
2017-02-06 09:59:17 -0700

    Tests: Add CreateJamVS2015Workspace to config_overrides

--- tests/config_overrides/CreateJamVS2010Workspace.bat
+++
@@ -1,2 +0,0 @@
-@jam --workspace --gen=vs2010 -gui --config=%~dp0CreateJamVS2010Workspace.config . build
-
---
+++ tests/config_overrides/CreateJamVS2015Workspace.bat
@@ -0,0 +1,2 @@
+@jam --workspace --gen=vs2015 -gui --config=%~dp0CreateJamVS2015Workspace.config . build
+
