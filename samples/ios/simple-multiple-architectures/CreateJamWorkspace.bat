58d6e254b185afcba1878babe00ff14a87cab348
Joshua Jensen <jjensen@workspacewhiz.com>
2017-02-06 12:05:15 -0700

    Tests: Enhance ios/simple-multiple-architectures with C.GetActiveToolchain

---
+++ samples/ios/simple-multiple-architectures/CreateJamWorkspace.bat
@@ -0,0 +1,1 @@
+@jam --workspace --gui --config=%~dp0CreateJamXcodeWorkspace.config Jamfile.jam %~dp0../../../ios/simple-multiple-architectures
--- samples/ios/simple-multiple-architectures/CreateJamXcodeWorkspace.bat
+++
@@ -1,1 +0,0 @@
-@jam --workspace --gui --gen=vs2013 --config=%~dp0CreateJamXcodeWorkspace.config Jamfile.jam %~dp0../../../ipad/simple
