07a132029a608a72af3049453058a48a32817dfb
Joshua Jensen <jjensen@workspacewhiz.com>
2018-02-02 06:56:32 -0700

    Bootstrapping: Fix up the clean-win##.cmd scripts

--- src/clean-win32.cmd
+++ src/clean-win32.cmd
@@ -1,4 +1,6 @@
 rmdir /s/q ..\bin\win32
+rmdir /s/q .build\win32-releaseltcg
+rmdir /s/q luaplus\.build\win32
 rmdir /s/q ipch
 rmdir /s/q obj.win32
 rmdir /s/q bin.ntx86
--- src/clean-win64.cmd
+++ src/clean-win64.cmd
@@ -1,4 +1,6 @@
 rmdir /s/q ..\bin\win64
+rmdir /s/q .build\win64-releaseltcg
+rmdir /s/q luaplus\.build\win64
 rmdir /s/q ipch
-rmdir /s/q obj
-rmdir /s/q bin.ntx86
+rmdir /s/q obj.win64
+rmdir /s/q bin.ntx64
