41dc0105ffaed1f4dd2d141da28f132f9e9a5d32
Josh Jensen <josh@everrush.com>
2020-05-05 11:38:32 -0600

    Samples: Update lua/helloworld/Jamfile.lua to use the new jam.C.Application syntax

--- samples/lua/helloworld/Jamfile.lua
+++ samples/lua/helloworld/Jamfile.lua
@@ -1,4 +1,5 @@
-jam["C.Application"]('helloworld', { '*.c', '*.h' })
+jam.C.Application('helloworld', { '*.c', '*.h' })
+--jam["C.Application"]('helloworld', { '*.c', '*.h' })
 
 -- Or use this:
 --jam_evaluaterule("C.Application", 'helloworld', { '*.c', '*.h' })
