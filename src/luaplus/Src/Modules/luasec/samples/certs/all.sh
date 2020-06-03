7d5604d422849a6b40de49603bd3e7befa457dcd
Josh Jensen <josh@everrush.com>
2020-06-03 11:32:13 -0600

    Remove unused Lua modules

--- src/luaplus/Src/Modules/luasec/samples/certs/all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-./rootA.sh
-./rootB.sh
-./clientA.sh
-./clientB.sh
-./serverA.sh
-./serverB.sh
--- src/luaplus/Src/Modules/lzlib/test_prologue.lua
+++
@@ -1,2 +0,0 @@
-print('lua version : ' .. _VERSION)
-print('zlib version: ' .. (require('zlib')._VERSION))
