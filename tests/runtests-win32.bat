0fc255a66a45b09cf46be22c068a1fe42b9d30f5
Josh Jensen <josh@everrush.com>
2020-06-11 12:39:11 -0600

    Tests: Fix runtests-win32.bat

--- tests/runtests-win32.bat
+++ tests/runtests-win32.bat
@@ -1,4 +1,1 @@
-@echo off
-setlocal
-set PATH=%~dp0..\bin\win32;%PATH%
-%~dp0..\bin\win32\lua\lua.exe %~dp0runtests.lua --platform win32 %*
+%~dp0..\bin\win32\jam.exe --runscript %~dp0runtests.lua --platform win32 %*
